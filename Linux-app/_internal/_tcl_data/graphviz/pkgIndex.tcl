dde4a773b1d46114ed390cfffb58e29f871208fb
iplanass <iplanass@pm.me>
2025-03-12 16:08:52 +0900

    remove linux app

--- Linux-app/_internal/_tcl_data/graphviz/pkgIndex.tcl
+++
@@ -1,9 +0,0 @@
-package ifneeded Tcldot 12.2.1 "
-	load [file join $dir libtcldot.so] Tcldot"
-package ifneeded Tclpathplan 12.2.1 "
-	load [file join $dir libtclplan.so] Tclpathplan"
-package ifneeded Gdtclft 12.2.1 "
-	load [file join $dir libgdtclft.so] Gdtclft"
-package ifneeded gv 0 "
-	load [file join $dir libgv_tcl.so] gv"
-# end
